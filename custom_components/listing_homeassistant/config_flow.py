cde776878056ef6a6bd48bcb4a9249d5ab30eb1b
Turiko313 <116017129+Turiko313@users.noreply.github.com>
2025-12-30 02:51:55 +0100

    Fix async_register_static_paths and config_entry setter issues

--- custom_components/listing_homeassistant/config_flow.py
+++ custom_components/listing_homeassistant/config_flow.py
@@ -49,7 +49,7 @@ class ListingHomeAssistantOptionsFlow(config_entries.OptionsFlow):
 
     def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
         """Initialize options flow."""
-        self.config_entry = config_entry
+        self._config_entry = config_entry
 
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
@@ -64,7 +64,7 @@ class ListingHomeAssistantOptionsFlow(config_entries.OptionsFlow):
                 {
                     vol.Required(
                         CONF_UPDATE_INTERVAL,
-                        default=self.config_entry.options.get(
+                        default=self._config_entry.options.get(
                             CONF_UPDATE_INTERVAL, UPDATE_INTERVALS["1_hour"]
                         ),
                     ): vol.In(
